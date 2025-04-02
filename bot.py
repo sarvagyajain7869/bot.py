94c805a755c321d9687c61c21956178b69b8a501
sarvagyajain7869 <sarvagyaj475@gmail.com>
2025-04-02 11:27:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-import random
+from selenium.webdriver.chrome.options import Options
 
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run in headless mode
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=chrome_options)
 chrome_options.add_argument("--headless")  # Run in background
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
